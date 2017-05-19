ee6e9c5c0cc1f4afd11f4dd2a75b8ac9220625d5
Kevin Dishman <kdishman@thoughtworks.com>
2017-05-19 13:36:06 -0500

    Added tests for different combat outcomes

--- test/combat_test.rb
+++ test/combat_test.rb
@@ -2,12 +2,29 @@ require 'test/unit'
 require_relative '../src/combat.rb'
 
 class CombatTest < Test::Unit::TestCase
-  def test_combat
+  def test_combat_minion_one_kills_minion_two_takes_damage
     m1 = Minion.new(5,3)
     m2 = Minion.new(1,1)
     m1, m2 = combat(m1, m2)
     assert_equal 5, m1.attack
     assert_equal 2, m1.hp
+    assert_nil m2
+  end
+
+  def test_combat_minion_two_kills_minion_one_no_damage_taken
+    m1 = Minion.new(0,2)
+    m2 = Minion.new(6,2)
+    m1, m2 = combat(m1, m2)
+    assert_equal 6, m2.attack
+    assert_equal 2, m2.hp
+    assert_nil m1
+  end
+
+  def test_combat_minions_kill_each_other
+    m1 = Minion.new(3,5)
+    m2 = Minion.new(6,2)
+    m1, m2 = combat(m1, m2)
+    assert_nil m1
     assert_nil m2
   end
 end
